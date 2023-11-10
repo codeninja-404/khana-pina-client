eadd980533669d760a3b31444d395645be2fde8f
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-11-10 21:02:38 +0600

    added category slider

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -4,30 +4,59 @@ import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 
-import slide1 from '../../../assets/home/slide1.jpg'
-import slide2 from '../../../assets/home/slide2.jpg'
-import slide3 from '../../../assets/home/slide3.jpg'
-import slide4 from '../../../assets/home/slide4.jpg'
-import slide5 from '../../../assets/home/slide5.jpg'
+import slide1 from "../../../assets/home/slide1.jpg";
+import slide2 from "../../../assets/home/slide2.jpg";
+import slide3 from "../../../assets/home/slide3.jpg";
+import slide4 from "../../../assets/home/slide4.jpg";
 
 const Category = () => {
   return (
     <Swiper
-    slidesPerView={'auto'}
-    centeredSlides={true}
-    spaceBetween={30}
-    pagination={{
-      clickable: true,
-    }}
-    modules={[Pagination]}
-    className="mySwiper"
+      slidesPerView={4}
+      spaceBetween={30}
+      loop={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="mySwiper mb-20"
     >
-      <SwiperSlide><img src={slide1} alt="" /></SwiperSlide>
-      <SwiperSlide><img src={slide2} alt="" /></SwiperSlide>
-      <SwiperSlide><img src={slide3} alt="" /></SwiperSlide>
-      <SwiperSlide><img src={slide4} alt="" /></SwiperSlide>
-      <SwiperSlide><img src={slide5} alt="" /></SwiperSlide>
-      
+      <SwiperSlide>
+        <img className="mx-auto" src={slide1} alt="" />
+        <p className="text-center text-4xl uppercase -mt-20 text-white ">
+          Salads
+        </p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="mx-auto" src={slide2} alt="" />
+        <p className="text-center text-4xl uppercase -mt-20 text-white ">
+          Pizzas
+        </p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="mx-auto" src={slide3} alt="" />
+        <p className="text-center text-4xl uppercase -mt-20 text-white ">
+          Soups
+        </p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="mx-auto" src={slide4} alt="" />
+        <p className="text-center text-4xl uppercase -mt-20 text-white ">
+          Desserts
+        </p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="mx-auto" src={slide3} alt="" />
+        <p className="text-center text-4xl uppercase -mt-20 text-white ">
+          Soups
+        </p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="mx-auto" src={slide2} alt="" />
+        <p className="text-center text-4xl uppercase -mt-20 text-white ">
+          Pizzas
+        </p>
+      </SwiperSlide>
     </Swiper>
   );
 };
