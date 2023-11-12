6bca0260d7bf99eeb594a61e6ee65dd9ae3e58ca
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-11-12 18:58:25 +0600

    geting menu data and reviews data from server

--- src/pages/Order/OrderTab/OrderTab.jsx
+++ src/pages/Order/OrderTab/OrderTab.jsx
@@ -2,7 +2,7 @@ import FoodCard from "../../Shared/FoodCard/FoodCard";
 
 const OrderTab = ({items}) => {
   return (
-    <div className="grid px-2 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 mx-auto gap-4">
+    <div className="grid w-96 sm:w-auto px-10 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 mx-auto gap-10">
       {items.map((item) => (
         <FoodCard key={item._id} item={item}></FoodCard>
       ))}
--- src/pages/Shared/FoodCard/FoodCard.jsx
+++ src/pages/Shared/FoodCard/FoodCard.jsx
@@ -2,18 +2,18 @@ const FoodCard = ({ item }) => {
   const { image, price, name, recipe } = item;
   return (
     <div>
-      <div className="card mx-auto rounded-none h-full  w-80 my-2 bg-yellow-100 shadow-xl">
+      <div className="card  rounded-none h-full my-2 bg-yellow-100 shadow-xl">
         
         <figure>
           <img src={image} alt="food" className="w-full"/>
         </figure>
         <p className="bg-slate-900 mr-4 mt-4 text-white px-4 py-1 rounded-lg absolute right-0">$ {price}</p>
        
-        <div className=" pt-4 px-4  text-center">
+        <div className="card-body  pt-4 px-4  text-center">
           <h2 className="uppercase block py-2 font-bold text-xl">{name}</h2>
           <p className="">{recipe }</p>
           <div className="card-actions justify-center ">
-            <button className="btn text-white px-7 mt-6 btn-ghost border-b-orange-500 border-b-4 bg-black  bg-opacity-60 hover:bg-black  hover:border-b-red-700  border-0">Add to cart</button>
+            <button className="btn text-white px-7  btn-ghost border-b-orange-500 border-b-4 bg-black  bg-opacity-60 hover:bg-black  hover:border-b-red-700  border-0">Add to cart</button>
           </div>
         </div>
       </div>
