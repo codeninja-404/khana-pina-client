40f5edfa317a20900da75ce4c3cbb75a845099fa
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-11-12 21:58:25 +0600

    added pagination for order

--- src/pages/Order/OrderTab/OrderTab.jsx
+++ src/pages/Order/OrderTab/OrderTab.jsx
@@ -1,11 +1,51 @@
 import FoodCard from "../../Shared/FoodCard/FoodCard";
 
-const OrderTab = ({items}) => {
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import { useState } from "react";
+
+const OrderTab = ({ items }) => {
+  const subarraySize = 3;
+
+  const createSubarrays = (array, size) => {
+    const subarrays = [];
+    for (let i = 0; i < array.length; i += size) {
+      subarrays.push(array.slice(i, i + size));
+    }
+    return subarrays;
+  };
+  const [result] = useState(createSubarrays(items, subarraySize));
+
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  };
+
   return (
-    <div className="grid w-96 sm:w-auto px-10 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 mx-auto gap-10">
-      {items.map((item) => (
-        <FoodCard key={item._id} item={item}></FoodCard>
-      ))}
+    <div>
+      <Swiper
+        pagination={pagination}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        {result.map((subarray, index) => (
+          <div key={index}>
+            <SwiperSlide>
+              <div className="grid w-96 sm:w-auto px-10 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 mx-auto gap-10">
+                {subarray.map((item) => (
+                  <FoodCard key={item._id} item={item}></FoodCard>
+                ))}
+              </div>
+            </SwiperSlide>
+          </div>
+        ))}
+      </Swiper>
     </div>
   );
 };
